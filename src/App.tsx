01a512b71397d5e73f5215a19b8b4a86d36d85bd
Garrett Weems <gwgraphicdesign@gmail.com>
2019-04-06 01:14:19 -0400

    homepage update

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
-import 'reset-css'
-import NavbarScroller from './components/'
+import 'reset-css';
+import NavbarScroller from './components/';
 
 const navigation = {
-  brand: { name: "NavScroller", to: "/" },
+  brand: { name: 'NavScroller', to: '/' },
   links: [
-    { name: "Item 1", to: "/" },
-    { name: "Item 2", to: "/" },
-    { name: "Item 3", to: "/" },
-    { name: "Item 4", to: "/" },
-    { name: "Item 5", to: "/" },
-    { name: "Item 6", to: "/" },
-    { name: "Item 7", to: "/" },
-    { name: "Item 8", to: "/" },
+    { name: 'Item 1', to: '/' },
+    { name: 'Item 2', to: '/' },
+    { name: 'Item 3', to: '/' },
+    { name: 'Item 4', to: '/' },
+    { name: 'Item 5', to: '/' },
+    { name: 'Item 6', to: '/' },
+    { name: 'Item 7', to: '/' },
+    { name: 'Item 8', to: '/' }
   ]
-}
+};
 
 export default class App extends Component {
   public render() {
@@ -24,9 +24,13 @@ export default class App extends Component {
       <div className="App">
         <NavbarScroller brand={brand} links={links} />
         <div className="hello">
-          <h1>React, TypeScript, Styled-Components!</h1></div>
+          <ul>
+            <li>React</li>
+            <li>TypeScript</li>
+            <li>Styled-Components</li>
+          </ul>
+        </div>
       </div>
     );
   }
 }
-
